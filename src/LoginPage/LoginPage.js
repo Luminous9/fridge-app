6f5e0bed8960ef9f666085b294ef1d5eaf0f59e8
Sonny <yihe939@gmail.com>
2017-06-14 07:45:43 -0400

    Turned on css modules.

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -12,9 +12,9 @@ googleProvider.setCustomParameters({
 export default class LoginPage extends Component {
     render() {
         return (
-            <div className="LoginPage">
+            <div className={styles.LoginPage}>
                 <h1>What's In My Fridge?</h1>
-                <p>Never lose track of what you have in your fridge, freezer, or pantry again.</p>
+                <p className={styles.subHeading}>Never lose track of what you have in your fridge, freezer, or pantry again.</p>
                 <div>
                     <button onClick={() => this.props.login(googleProvider)}>Login With Google</button>
                 </div>
